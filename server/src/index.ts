d038d2049308e3ee3360395d68f328d18a9f9892
Manny <manny1_dev@tutamail.com>
2025-03-03 01:16:55 +0000

    node server init

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
+
+// Create a new express application instance
+const app = express();
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:4200", "http://172.17.0.2:4200"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
+app.use(cors(corsOptions));
+
+// Set the network port
+const port = process.env.PORT || 3000;
+
+// Define the root path with a greeting message
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the Express + TypeScript Server!" });
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`);
+});
