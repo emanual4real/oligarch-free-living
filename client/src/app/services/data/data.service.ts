4d99ee6ae01297a0dbf3d09e572e93a439f650a5
Manny <manny1_dev@tutamail.com>
2025-04-01 00:21:03 +0000

    cache api responses

--- client/src/app/services/data/data.service.ts
+++ client/src/app/services/data/data.service.ts
@@ -2,25 +2,62 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environment';
 import { Company, Oligarch, Product, Project2025 } from '@types';
+import { map, Observable, of, switchMap, tap } from 'rxjs';
+
+interface DataCache {
+  oligarchs: Oligarch[];
+  products: Product[];
+  companies: Company[];
+  project2025: Project2025[];
+}
+
+type DataCacheKey = keyof DataCache;
+
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
+  private cache: DataCache = {
+    oligarchs: [],
+    products: [],
+    companies: [],
+    project2025: [],
+  };
+
   constructor(private httpClient: HttpClient) {}
 
+  private getCache(key: DataCacheKey) {
+    return of(this.cache).pipe(map((data) => data[key]));
+  }
+
+  private cacheOrFetchData<T>(key: DataCacheKey) {
+    return this.getCache(key).pipe(
+      switchMap((data) => {
+        if (data.length > 0) {
+          return of(data) as Observable<T[]>;
+        }
+        return this.httpClient.get<T[]>(`${environment.apiUrl}/${key}`).pipe(
+          tap((data) => {
+            this.cache[key] = data as any;
+          })
+        );
+      })
+    );
+  }
+
   getOligarchs() {
-    return this.httpClient.get<Oligarch[]>(`${environment.apiUrl}/oligarchs`);
+    return this.cacheOrFetchData<Oligarch>('oligarchs');
   }
 
   getProducts() {
-    return this.httpClient.get<Product[]>(`${environment.apiUrl}/products`);
+    return this.cacheOrFetchData<Product>('products');
   }
 
   getCompanies() {
-    return this.httpClient.get<Company[]>(`${environment.apiUrl}/companies`);
+    return this.cacheOrFetchData<Company>('companies');
   }
 
   getProject2025Data() {
-    return this.httpClient.get<Project2025[]>(`${environment.apiUrl}/project2025`);
+    return this.cacheOrFetchData<Project2025>('project2025');
   }
 }
