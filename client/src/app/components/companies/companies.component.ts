f9556526fca805a4979e7f72517ac01cb14f549b
Manny <manny1_dev@tutamail.com>
2025-04-03 02:38:16 +0000

    add company table

--- client/src/app/components/companies/companies.component.ts
+++ client/src/app/components/companies/companies.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DataService } from '@services';
+import { Company } from '@types';
+import { TableModule } from 'primeng/table';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-companies',
-  imports: [],
+  imports: [TableModule, CommonModule],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.css',
 })
-export class CompaniesComponent {}
+export class CompaniesComponent {
+  data$: Observable<Company[]> = this.dataService.getCompanies();
+
+  constructor(private dataService: DataService) {}
+}
