238469b363c7fe01eb3576856298f5c610bf0f07
Manny <manny1_dev@tutamail.com>
2025-04-03 02:40:02 +0000

    fix test

--- client/src/app/components/companies/companies.component.spec.ts
+++ client/src/app/components/companies/companies.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { DataService } from '@services';
 import { CompaniesComponent } from './companies.component';
 
 describe('CompaniesComponent', () => {
@@ -9,6 +12,7 @@ describe('CompaniesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CompaniesComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting(), DataService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CompaniesComponent);
