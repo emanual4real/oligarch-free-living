ae196010aaa489c196abc7fa394673591235f9c2
Paul Cain <paul@cain-design.com>
2025-04-12 01:36:33 +0000

    Intro front end work

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SearchComponent } from '../search';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule, CheckboxModule, MultiSelectModule, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  // just a javascript class
+
+  cat = 'meow';
+  dog: string = 'woof';
+
+  doStuff() {
+    console.log('doing something');
+  }
+}
