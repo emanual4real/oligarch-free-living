11dbf15f2409b7a2c5d5372e6420ffa3a30c7313
manny <manny1_dev@tutamail.com>
2025-04-12 03:51:32 +0000

    WIP: responsive tables

--- client/src/app/components/oligarchs/oligarch-table.component.ts
+++ client/src/app/components/oligarchs/oligarch-table.component.ts
@@ -1,24 +1,90 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DataService } from '@services';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
-import { map, take } from 'rxjs';
+import { distinctUntilChanged, map, take, tap } from 'rxjs';
 
+type Breakpoint = 'XSmall' | 'Small' | 'Medium' | 'Large';
+
+interface Column {
+  field: string;
+  header: string;
+  type: 'array' | 'text';
+  sort?: boolean;
+  breakpoint?: Breakpoint[];
+}
 @Component({
   selector: 'app-oligarch-table',
   imports: [TableModule, ToastModule, ButtonModule, CommonModule, RouterLink],
   templateUrl: './oligarch-table.component.html',
   styleUrl: './oligarch-table.component.css',
 })
-export class OligarchTableComponent {
+export class OligarchTableComponent implements OnInit {
   data$ = this.dataService.getOligarchs();
   expandedRows$ = this.data$.pipe(map((data) => data.map((row) => row.sources)));
   expandedRows = {};
 
-  constructor(private dataService: DataService) {}
+  cols: Column[] = [
+    {
+      header: 'Name',
+      field: 'name',
+      type: 'text',
+      sort: true,
+    },
+    {
+      header: 'Companies',
+      field: 'companies',
+      type: 'array',
+    },
+    {
+      header: 'Description',
+      field: 'description',
+      type: 'text',
+      breakpoint: ['Medium', 'Large'],
+    },
+    // {
+    //   header: 'Sources',
+    //   field: 'sources',
+    //   type: 'array',
+    // },
+  ];
+
+  Breakpoints = Breakpoints;
+  currentBreakpoint!: Breakpoint;
+
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, Breakpoints.XSmall])
+    .pipe(
+      tap((value) => {
+        console.log(value);
+      }),
+      distinctUntilChanged()
+    );
+
+  constructor(
+    private dataService: DataService,
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    this.breakpoint$.subscribe(() => this.breakpointChanged());
+  }
+
+  private breakpointChanged() {
+    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.currentBreakpoint = 'Large';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = 'Medium';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Small)) {
+      this.currentBreakpoint = 'Small';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.XSmall)) {
+      this.currentBreakpoint = 'XSmall';
+    }
+  }
 
   expandAll() {
     this.data$
