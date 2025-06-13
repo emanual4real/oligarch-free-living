e0b3b406ba4e712426ea84e32167803f178fc1f2
manny <manny1_dev@tutamail.com>
2025-06-13 02:57:51 +0000

    fix front page banner

--- client/src/app/services/photo/photo.service.ts
+++ client/src/app/services/photo/photo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environment';
+import { map } from 'rxjs';
 import { PhotoGallery } from 'src/app/types/photos';
 
 @Injectable({
@@ -10,6 +11,8 @@ export class PhotoService {
   constructor(private readonly httpClient: HttpClient) {}
 
   getData() {
-    return this.httpClient.get<PhotoGallery[]>(`${environment.apiUrl}/photos`);
+    return this.httpClient
+      .get<PhotoGallery[]>(`${environment.apiUrl}/photos`)
+      .pipe(map((data) => data.map((row) => ({ ...row, itemImageSrc: `${environment.apiUrl}/${row.itemImageSrc}` }))));
   }
 }
--- server/src/routes/photo.routes.ts
+++ server/src/routes/photo.routes.ts
@@ -8,7 +8,6 @@ export const photoRouter = express.Router();
 photoRouter.get("/", async (req: Request, res: Response) => {
   const fileList: string[] = [];
   const directory = config.photoPath;
-  const port = config.port;
 
   if (directory) {
     readdir(directory, (err, files) => {
@@ -21,7 +20,7 @@ photoRouter.get("/", async (req: Request, res: Response) => {
 
       const result = fileList.map((row) => {
         const photoGallery: PhotoGallery = {
-          itemImageSrc: `http://localhost:${port}/images/${row}`,
+          itemImageSrc: `images/${row}`,
           thumbnailImageSrc: "",
           alt: "",
           title: "",
